e96b2ac1984886fc68d46f93314ab524209ffdb5
kkzall <2311569301@qq.com>
2020-02-16 20:55:20 +0800

    Array.java

--- Main.java
+++ Main.java
@@ -1,17 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] arr = new int[20];
-        for (int i = 0 ; i < arr.length ; i++)
-            arr[i] = i;
-
-        int[] scores = new int[]{100, 99, 66};
-        for(int i = 0 ; i < scores.length ; i ++)
-            System.out.println(scores[i]);
-
-        for (int score: scores)
-            System.out.println(score);
-	// write your code here
+        Array arr = new Array(20);
+        for (int i = 0 ; i < 10 ; i ++)
+            arr.addLast(i);
+        System.out.println(arr);
     }
 }
