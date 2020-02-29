59f043a290e0394fea6bbaffdb16c1912ccc23bd
kkzall <2311569301@qq.com>
2020-02-29 19:24:55 +0800

    LickedList

--- LinkedList/src/LiskedList.java
+++ LinkedList/src/LiskedList.java
@@ -1,35 +1,71 @@
 public class LiskedList<E> {
-    private class Node{
+    private class Node {
         private E e;
         private Node next;
-` `
-        public Node(E e , Node next){
+
+        public Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
-        public Node(E e){
-            this(e,null);
+
+        public Node(E e) {
+            this(e, null);
         }
-        public Node(){
-            this(null,null);
+
+        public Node() {
+            this(null, null);
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return e.toString();
         }
-        }
+    }
+
     private Node head;
     private int size;
-    public  LiskedList(){
+
+    public LiskedList() {
         head = null;
         size = 0;
     }
+
     //获取链表中的元素个数
-    public int getSize(){
+    public int getSize() {
         return size;
     }
+
     //返回链表是否为空
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
+    //在链表头添加元素e
+    public void addFirst(E e){
+    //    Node node = new Node(e);
+    //    node.next = head;
+    //    head = node;
+    head = new Node(e,head);
+    size ++;
+
+    }
+    //在链表的index（0-based）位置添加新的元素e
+    //在链表中不是一个常用的操作，练习用；）
+    public void add(int index,E e){
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add faild.Illegal index.");
+        if (index == 0)
+            addFirst(e);
+        else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++)
+                prev = prev.next;
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+    }
+    //在链表末尾添加元素
+    public void addLast(E e){
+        add(size,e);
+    }
 }
+
