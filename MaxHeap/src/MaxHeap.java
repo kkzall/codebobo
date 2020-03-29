ca4f1fef7881d80370fdf879baf8f1fec0867423
kkzall <2311569301@qq.com>
2020-03-29 22:20:45 +0800

    MaxHeap

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-
 public class MaxHeap<E extends Comparable<E>> {
     private Array<E> data;
     public MaxHeap(int capacity){
@@ -7,6 +5,11 @@ public class MaxHeap<E extends Comparable<E>> {
     }
     public MaxHeap(){
         data = new Array<>();
+    }
+    public MaxHeap(E[] arr){
+        data = new Array<>(arr);
+        for (int i = parent(arr.length - 1) ; i >= 0 ; i --)
+            siftDown(i);
     }
     //返回堆中的元素个数
     public int size(){
@@ -17,7 +20,7 @@ public class MaxHeap<E extends Comparable<E>> {
         return data.isEmpty();
     }
     //返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
-    private int parant(int index){
+    private int parent(int index){
         if (index == 0)
             throw new IllegalArgumentException("index-0 doesn't have parent.");
         return (index -1)/2;
@@ -30,4 +33,49 @@ public class MaxHeap<E extends Comparable<E>> {
     private int rightChild(int index){
         return index * 2 + 2;
     }
+    //向堆中添加元素
+    public void add (E e){
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+    private void siftUp(int k){
+        while(k > 0 && data.get(parent(k)).compareTo(data.get(k))<0) {
+            data.swap(k,parent(k));
+            k=parent(k);
+        }
+    }
+    //看堆中的最大元素
+    public E findMax(){
+        if (data.getSize() == 0)
+            throw new IllegalArgumentException("Can not findMax  when heap is empty");
+        return data.get(0);
+    }
+    //取出堆中的最大元素
+    public E extractMax(){
+        E ret = findMax();
+        data.swap(0,data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+
+        return ret;
+    }
+    private void siftDown(int k){
+        while (leftChild(k) < data.getSize()){
+           int j = leftChild(k);
+           if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j))>0)
+               j = rightChild(k);
+           //data[j] 是leftChild 和 rightChild中的最大值
+            if (data.get(k).compareTo(data.get(j)) > 0)
+                break;
+            data.swap(k,j);
+            k = j;
+        }
+    }
+    public E replace(E e){
+        E ret = findMax();
+        data.set(0,e);
+        siftDown(0);
+        return ret;
+    }
 }
+
